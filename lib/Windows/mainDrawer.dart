f9d1997dc654150631817c9d6016afd9e0efe713
ManyuDhyani <manyudhyani@gmail.com>
2022-02-09 12:57:04 +0530

    Setup Main Drawer and Badge for notification

---
+++ lib/Windows/mainDrawer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class mainDrawer extends StatelessWidget {
+  const mainDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer();
+  }
+}
--- lib/widgets/mainNav.dart
+++ lib/widgets/mainNav.dart
@@ -1,11 +1,13 @@
 import 'package:facebook_ui_clone_flutter/Windows/friendDetail.dart';
 import 'package:facebook_ui_clone_flutter/Windows/homeDetail.dart';
+import 'package:facebook_ui_clone_flutter/Windows/mainDrawer.dart';
 import 'package:facebook_ui_clone_flutter/Windows/marketPlaceDetail.dart';
 import 'package:facebook_ui_clone_flutter/Windows/messageDetail.dart';
 import 'package:facebook_ui_clone_flutter/Windows/notificationDetail.dart';
 import 'package:facebook_ui_clone_flutter/Windows/videoDetail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:badges/badges.dart';
 
 class NavMain extends StatefulWidget {
   NavMain({Key? key}) : super(key: key);
@@ -15,11 +17,18 @@ class NavMain extends StatefulWidget {
 }
 
 class _NavMainState extends State<NavMain> with SingleTickerProviderStateMixin {
-  final List<Tab> topNavTabs = const <Tab>[
+  final List<Tab> topNavTabs = <Tab>[
     Tab(icon: Icon(Icons.home_outlined)),
     Tab(icon: Icon(Icons.people_outline)),
     Tab(icon: Icon(Icons.message_outlined)),
-    Tab(icon: Icon(Icons.notifications_outlined)),
+    Tab(
+      icon: Badge(
+        position: BadgePosition.topEnd(top: -20, end: -5),
+        badgeContent:
+            Text("3", style: TextStyle(fontSize: 18, color: Colors.white)),
+        child: Icon(Icons.notifications_outlined),
+      ),
+    ),
     Tab(icon: Icon(Icons.video_library_outlined)),
     Tab(icon: Icon(Icons.shopping_bag_outlined)),
   ];
@@ -47,11 +56,14 @@ class _NavMainState extends State<NavMain> with SingleTickerProviderStateMixin {
     return _tabController.index == 0;
   }
 
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
           title: Text(
             'facebook',
@@ -83,7 +95,7 @@ class _NavMainState extends State<NavMain> with SingleTickerProviderStateMixin {
                 splashColor: Colors.transparent,
                 highlightColor: Colors.transparent,
                 onPressed: () {
-                  print("Menu button is pressed");
+                  _scaffoldKey.currentState?.openEndDrawer();
                 },
               ),
               decoration: BoxDecoration(
@@ -97,6 +109,10 @@ class _NavMainState extends State<NavMain> with SingleTickerProviderStateMixin {
             indicatorColor: Colors.black,
             tabs: topNavTabs,
           ),
+        ),
+        endDrawer: Container(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: mainDrawer(),
         ),
         body: TabBarView(controller: _tabController, children: [
           homeDetail(),
